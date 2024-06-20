a19bd9939c95c97502bab01ee86acda90b10af10
prajith10rocks <prajithradhakrishnanar123@gmail.com>
2024-06-20 10:29:27 +0530

    changed main

--- task-manager/app.js
+++ task-manager/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app=express();
 const bodyparser = require('body-parser');
 const dotenv = require('dotenv');
+const handleBars = require('express-handlebars');
 const path = require('path');
 const mongoose = require('mongoose');
 dotenv.config({path: path.resolve(__dirname, 'envfile.env')});
@@ -10,11 +11,27 @@ app.listen(port,()=>{
     console.log("SERVER IS LISTENING  ON PORT "+port);
 })
 
+
+//static files
+const publicDirectoryPath = path.resolve(__dirname,'public');
+app.use(express.static(publicDirectoryPath));
+
 //middleware
 app.use(bodyparser.urlencoded({extended:false}));
 app.use(bodyparser.json());
 
+
+//set view engine
+const hbs = handleBars.create({extname:'.hbs',});
+
+//Register the handlebars engine with the Express app
+app.engine('.hbs',hbs.engine);
+app.set('view engine', '.hbs');
+
+//mongodb connection
 mongoose.connect("mongodb+srv://Nproject:newproject@testcluster.udr6vli.mongodb.net/").then(() => {
     console.log("DATABASE CONNECTED");
+}).catch((err) => {
+    console.log("database connection failed "+err);
 })
 
